6bddac1df57b8a8362cee93c9bd7357e102a17e9
Nelson de Sousa Soares <nelsonssoares@ibm.com>
2023-05-17 18:18:06 -0300

    Implementando teste em UsuarioService

--- src/test/java/com/ibmshop/userapi/commons/usuarios/UsuariosConstant.java
+++ src/test/java/com/ibmshop/userapi/commons/usuarios/UsuariosConstant.java
@@ -3,23 +3,32 @@ package com.ibmshop.userapi.commons.usuarios;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import com.ibmshop.userapi.domain.dto.EnderecoDTO;
+import com.ibmshop.userapi.domain.dto.PaisDTO;
 import com.ibmshop.userapi.domain.dto.UsuarioDTO;
 import com.ibmshop.userapi.domain.entities.Endereco;
 import com.ibmshop.userapi.domain.entities.Usuario;
 import com.ibmshop.userapi.domain.enums.Pergunta;
 
 public class UsuariosConstant {
-	static List<EnderecoDTO> enderecoDto = new ArrayList<>();
+	public static final PaisDTO PAIS = new PaisDTO("Brasil","+055");
+	public static final EnderecoDTO ENDTEST= new EnderecoDTO("Av paulista","1000","AP 11","02636-030","Parada inglesa","São Paulo","São Paulo","Casa",PAIS,Pergunta.SIM);
+	public static final List<EnderecoDTO> ENDDTO =  new ArrayList<>();
+	//public static final List<EnderecoDTO> ENDDTO1 = new ArrayList<>();
 	static List<Endereco> endereco = new ArrayList<>();
+	public static final Optional<Usuario> USUARIOOP = Optional.of(new Usuario(28,"Nelson","Soares","420.197.488-00","+055 (11) 97959-3152",LocalDate.now(),LocalDate.now(),Pergunta.SIM,endereco));
 	public static final Usuario USUARIO = new Usuario(28,"Nelson","Soares","420.197.488-00","+055 (11) 97959-3152",LocalDate.now(),LocalDate.now(),Pergunta.SIM,endereco);
-	public static final UsuarioDTO USUARIODTO = new UsuarioDTO("Nelson","Soares","420.197.488-00",enderecoDto,"+055 (11) 97959-3152");
+	public static final UsuarioDTO USUARIODTO = new UsuarioDTO("Nelson","Soares","420.197.488-00",ENDDTO,"+055 (11) 97959-3152");
 	public static final UsuarioDTO INVALID_USUARIODTO = new UsuarioDTO();
-	
+	public static final Usuario INVALID_USUARIO = new Usuario();
+		
 }
+
+//{"Casa","Av paulista","1000","AP 11","02636-030","Parada inglesa","São Paulo","São Paulo",pais,Pergunta.SIM}
 /*
-TIPO USUARIO 
+	USUARIO 
 	this.id = id;
 		this.nome = nome;
 		this.sobrenome = sobrenome;
@@ -30,3 +39,20 @@ TIPO USUARIO
 		this.ativo = ativo;
 		this.endereco = endereco;
 */
+
+/*			
+ *			ENDERECODTO
+			"rua": "Av paulista",
+            "numero": "1000",
+            "complemento": "AP 11",
+            "cep": "02636-030",
+            "bairro": "Parada inglesa",
+            "cidade": "São Paulo",
+            "estado": "São Paulo",
+            "enderecoPadrao": 0,
+            "apelido": "Casa",
+            "pais": {
+               "nome": "Brasil",
+               "codigo": "+055"
+            } 
+ */
--- src/test/java/com/ibmshop/userapi/unittest/usuario/UsuarioServiceTest.java
+++ src/test/java/com/ibmshop/userapi/unittest/usuario/UsuarioServiceTest.java
@@ -1,14 +1,29 @@
 package com.ibmshop.userapi.unittest.usuario;
 
+
+
+import static com.ibmshop.userapi.commons.usuarios.UsuariosConstant.INVALID_USUARIODTO;
+import static com.ibmshop.userapi.commons.usuarios.UsuariosConstant.USUARIO;
+import static com.ibmshop.userapi.commons.usuarios.UsuariosConstant.USUARIOOP;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ibmshop.userapi.domain.entities.Usuario;
 import com.ibmshop.userapi.domain.repository.Usuarios;
-import com.ibmshop.userapi.service.impl.UsuarioServiceImpl;;
+import com.ibmshop.userapi.service.impl.UsuarioServiceImpl;
 
 @ExtendWith(MockitoExtension.class)
 public class UsuarioServiceTest {
@@ -23,47 +38,51 @@ public class UsuarioServiceTest {
 	@Test
 	public void createUsuario_WithValidData_ReturnsUsuario() {
 		
-		/*	
-        when(usuarioRepository.findById(anyInt())).thenReturn(Optional.of(USUARIO));
-        
-        Optional<Usuario> sut = usuarioService.buscarPorId(1);
-        
-        assertThat(sut).isNotEmpty();
-        assertThat(sut.buscarPorId()).isEqualTo(PLANET);
-	*/
-
+		usuarioRepository.save(USUARIO);
+		assertThat(USUARIO).isEqualTo(USUARIO);
+		
+		/* o Teste passou, mas acredito estar errado, pois não ouve a chamada do service
+		 * do tipo UsuarioDTO.
+		 * Confusão na hora de chaamar o service(UserioDTO) e repository(Usuario)
+		 */
+		
+	  	/*PROBLEMA COM A CONVERSÃO DE DADOS 
+			 * USUARIODTO -> USUARIO
+			 * USUARIO SERVICE ACEITA APENAS USUARIODTO COMO PARAMETRO,
+			 * E USUARIO REPOSITORY ACEITA APENAS TIPO USUARIO
+		*/
 	}
 
 	
 	@Test
     public void createUsuario_WithInValidData_ThrowsException(){
-       	/*PROBLEMA COM A CONVERSÃO DE DADOS 
-		 * USUARIODTO -> USUARIO
-		 * USUARIO SERVICE ACEITA APENAS USUARIODTO COMO PARAMETRO,
-		 * E USUARIO REPOSITORY ACEITA APENAS TIPO USUARIO
-		*/
+		
+		//when(usuarioRepository.save(INVALID_USUARIO)).thenThrow(RuntimeException.class);
+		
+		assertThatThrownBy(()-> usuarioService.salvar(INVALID_USUARIODTO)).isInstanceOf(RuntimeException.class);
+     
     }
 	@Test
-    public void getUsuario_ByExistingId_ReturnsPlanet(){
-	/*	
-        when(usuarioRepository.findById(anyInt())).thenReturn(Optional.of(USUARIO));
+    public void getUsuario_ByExistingId_ReturnsUsuario(){
+		
+        when(usuarioRepository.findById(anyInt())).thenReturn(USUARIOOP);
         
-        Optional<Usuario> sut = usuarioService.buscarPorId(1);
+        ResponseEntity<Usuario> sut = usuarioService.buscarPorId(1);
         
-        assertThat(sut).isNotEmpty();
-        assertThat(sut.buscarPorId()).isEqualTo(PLANET);
-	*/
+        assertThat(sut).isNotNull();
+        assertThat(sut.getBody()).isEqualTo(USUARIO);
+	
     }   
 
     @Test
     public void getPlanet_ByUnexistingId_ReturnsEmpty(){
-	/*
-        when(planetRepository.findById(1L)).thenReturn(Optional.empty());
+	
+        when(usuarioRepository.findById(2042)).thenReturn(Optional.empty());
 
-        Optional<Planet> sut = planetService.get(1L);
+        ResponseEntity<Usuario> sut = usuarioService.buscarPorId(2042);
 
-        assertThat(sut).isEmpty();
-	*/
+        assertThat(sut).isEqualTo(ResponseEntity.notFound().build());
+	
     }
 
 
