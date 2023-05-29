5fee8e31e9d3023d3b8ad35fcff925292df12c7c
Nelson de Sousa Soares <nelsonssoares@ibm.com>
2023-05-29 11:23:34 -0300

    Iniciando CRUD de enderecos

--- src/main/java/com/ibmshop/userapi/rest/controller/EnderecoController.java
+++ src/main/java/com/ibmshop/userapi/rest/controller/EnderecoController.java
@@ -1,36 +1,57 @@
 package com.ibmshop.userapi.rest.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ibmshop.userapi.domain.dto.EnderecoDTO;
 import com.ibmshop.userapi.domain.entities.Endereco;
 import com.ibmshop.userapi.service.EnderecoService;
 
+
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @Tag(name = "IBM Shop - Shopping API")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("ibm/enderecos")
-public class EnderecoController implements EnderecoService {
+public class EnderecoController {
 	
-	@Override
-	public ResponseEntity<Endereco> salvar(EnderecoDTO endDto) {
+	private final EnderecoService service;
+	
+	@GetMapping
+	public ResponseEntity<List<Endereco>> getAll(){
+		return null;
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<Endereco> save(@RequestBody @Valid EnderecoDTO endDto) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
-	public ResponseEntity<Endereco> atualizar(Integer id, EnderecoDTO endDto) {
+	@PutMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public ResponseEntity<Endereco> update(@PathVariable("id") Integer id,@RequestBody @Valid EnderecoDTO endDto) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
-	public ResponseEntity<Endereco> deletar(Integer id) {
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Endereco> delete(@PathVariable("id") Integer id) {
 		// TODO Auto-generated method stub
 		return null;
 	}
--- src/main/java/com/ibmshop/userapi/service/impl/EnderecoServiceImpl.java
+++ src/main/java/com/ibmshop/userapi/service/impl/EnderecoServiceImpl.java
@@ -1,11 +1,13 @@
 package com.ibmshop.userapi.service.impl;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
 import com.ibmshop.userapi.domain.dto.EnderecoDTO;
 import com.ibmshop.userapi.domain.entities.Endereco;
 import com.ibmshop.userapi.service.EnderecoService;
 
+@Service
 public class EnderecoServiceImpl implements EnderecoService {
 
 	@Override
